ff11a43090cc634556b048b574ca5a168551d5fc
Rabeet Sati <rabeetsati@gmail.com>
2023-07-08 00:20:46 +0500

    Added Categories + Ratings Filter

--- Complete MERN App/front-end/src/actions/productAction.js
+++ Complete MERN App/front-end/src/actions/productAction.js
@@ -9,12 +9,19 @@ import {
     CLEAR_ERRORS
 } from '../constants/productConstants'
 
-const getProducts = (keyword = '', currentPage = 1, price = [0, 25000]) => async (dispatch) => {
+const getProducts = (keyword = '', currentPage = 1, price = [0, 25000], category, ratings = 0) => async (dispatch) => {
 
     try {
 
         dispatch({ type: ALL_PRODUCT_REQUEST });
-        const { data } = await axios.get(`http://localhost:5000/api/v1/product?keyword=${keyword}&page=${currentPage}&price[gt]=${price[0]}&price[lt]=${price[1]}`);
+
+        let link = `http://localhost:5000/api/v1/product?keyword=${keyword}&page=${currentPage}&price[gt]=${price[0]}&price[lt]=${price[1]}&ratings[gte]=${ratings}`;
+
+        if (category) {
+            link = `http://localhost:5000/api/v1/product?keyword=${keyword}&page=${currentPage}&price[gt]=${price[0]}&price[lt]=${price[1]}&category=${category}&ratings[gte]=${ratings}`
+        }
+
+        const { data } = await axios.get(link);
 
         dispatch({
             type: ALL_PRODUCT_SUCCESS,
--- Complete MERN App/front-end/src/components/Product/Products.js
+++ Complete MERN App/front-end/src/components/Product/Products.js
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from "react";
 import './Products.css'
 import { useSelector, useDispatch } from 'react-redux'
-import { getProducts, clearErrors } from '../../actions/productAction'
+import { getProducts } from '../../actions/productAction'
 import Loader from "../layout/loader/Loader";
 import ProductCard from "../Home/ProductCard";
 import { useParams } from "react-router-dom";
 import Pagination from 'react-js-pagination';
 import Slider from '@mui/material/Slider';
 import { Typography } from "@mui/material";
+import MetaData from "../layout/MetaData";
 
+const Categories = [
+    'Laptop',
+    'Footwear',
+    'Bottom',
+    'Tops',
+    'Attire',
+    'Camera',
+    'SmartPhones',
+]
 
 
 const Products = ({ match }) => {
@@ -17,6 +27,8 @@ const Products = ({ match }) => {
     const { keyword } = useParams()
     const [currentPage, setCurrrentPage] = useState(1)
     const [price, setPrice] = useState([0, 25000])
+    const [category, setCategory] = useState('')
+    const [ratings, setRatings] = useState(0)
     const { products, loading, error, productsCount, productPerPage } = useSelector(state => state.products)
 
     const setCurrrentPageNo = (e) => {
@@ -29,13 +41,13 @@ const Products = ({ match }) => {
 
     useEffect(() => {
 
-        dispatch(getProducts(keyword, currentPage, price))
+        dispatch(getProducts(keyword, currentPage, price, category, ratings))
 
-    }, [dispatch, keyword, error, currentPage, price]);
+    }, [dispatch, keyword, error, currentPage, price, category, ratings]);
 
     return <>
         {loading ? <Loader /> : <>
-
+            <MetaData title={'PRODUCTS -- ECOMMERCE'} />    
             <h2 className="productsHeading">Products</h2>
             <div className="products">
                 {products &&
@@ -58,6 +70,31 @@ const Products = ({ match }) => {
                     min={0}
                     max={25000}
                 />
+
+                <Typography>Categories</Typography>
+
+                <ul className="categoryBox">
+                    {Categories.map((category) => (
+                        <li className="category-link" key={category} onClick={() => setCategory(category)}>
+                            {category}
+                        </li>
+                    ))}
+                </ul>
+
+                <fieldset >
+                    <Typography component={'legend'}>Ratings above</Typography>
+                    <Slider
+                        value={ratings}
+                        onChange={(e, newRating) => {
+                            setRatings(newRating)
+                        }}
+                        aria-labelledby="continuous-slider"
+                        valueLabelDisplay="auto"
+                        color="warning"
+                        min={0}
+                        max={5}
+                    />
+                </fieldset>
 
             </div>
 
